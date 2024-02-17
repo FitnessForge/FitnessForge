84ae70cac44250069c3bcc03ff43bec020f0cae9
Anddy Labrut <73158252+Andduril@users.noreply.github.com>
2024-02-18 00:17:58 +0100

    Add PWA

--- apps/front/src/app/layout.tsx
+++ apps/front/src/app/layout.tsx
@@ -3,6 +3,53 @@ import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 import theme from '../theme';
 import { CssBaseline } from '@mui/material';
 import { Providers } from '@lib/providers';
+import type { Metadata, Viewport } from "next";
+
+const APP_NAME = "Fitness Fusion";
+const APP_DEFAULT_TITLE = "Fitness Fusion";
+const APP_TITLE_TEMPLATE = "%s - Fitness Fusion";
+const APP_DESCRIPTION = "";
+
+export const metadata: Metadata = {
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
+};
+
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (

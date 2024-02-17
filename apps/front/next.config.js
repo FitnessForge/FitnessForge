10c0b96f78a1e6eb777f7241c9dc5884801f500f
Anddy Labrut <73158252+Andduril@users.noreply.github.com>
2024-02-18 00:21:01 +0100

    disable PWA reload for develop

--- apps/front/next.config.js
+++ apps/front/next.config.js
@@ -3,6 +3,9 @@ const { composePlugins, withNx } = require('@nx/next');
 const withPWA = require('next-pwa')(
   {
     dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
   }
 )
 

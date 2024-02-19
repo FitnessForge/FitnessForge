adec92809bcbf82bb5bbade47a989ba1d823e252
Axel Gil <axelgil60@gmail.com>
2024-02-19 20:59:30 +0100

    merged develop

--- apps/front-e2e/cypress.config.ts
+++ apps/front-e2e/cypress.config.ts
@@ -2,12 +2,14 @@ import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 
 import { defineConfig } from 'cypress';
 
+const port = process.env.FRONT_PORT || 3000;
+
 export default defineConfig({
   e2e: {
     ...nxE2EPreset(__filename, {
       cypressDir: 'src',
       webServerCommands: { default: 'nx run front:serve' },
     }),
-    baseUrl: 'http://localhost:3000',
+    baseUrl: `http://localhost:${port}`,
   },
 });
